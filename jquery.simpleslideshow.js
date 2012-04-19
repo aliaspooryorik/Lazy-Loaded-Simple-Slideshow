db10310aa520636abbfc76c28c896067ea1e7260
John Whish <john.whish@gmail.com>
2012-04-19 16:47:25 +0100

    wrote the plugin to create an instance and a basic API

--- jquery.simpleslideshow.js
+++ jquery.simpleslideshow.js
@@ -9,71 +9,151 @@
 (function( $ ) {
 
 	$.fn.simpleSlideshow = function( options ) {
-		// default options
-		var settings = $.extend( {
-			'slideshowData'			: [],
-			'slideFadeSpeed'		: 2000,
-			'slidePauseSpeed'		: 4000,
-			'paging'				: true
-		}, options );
-	
-		// refer to settings as settings.xyz 
-		var $this = $(this);
-		
-		// get the number of additional images
-		var slideshowlen = settings.slideshowData.length;
-	
-		// check that the array has items and the slideshow container exists in the page
-		if ($this.length > 0 && slideshowlen > 0){
-			
-			// loop and append additional images
-			$.each(settings.slideshowData, function(ndx, el){
-				if ($.isPlainObject(el)) {
-					var banner = '<img src="' + el.src + '" alt="' + el.alt + '">';
-					if (el.href) {
-						banner = '<a href="' + el.href + '">' + banner + '</a>';
-					}
-				} 
-				else {
-					var banner = el;
-				}
-				$placeholder = $('<div>').hide().append(banner);
-				$this.append($placeholder);
-				
+		if ( methods[options] ) {
+			// calling a method;
+			var args = Array.prototype.slice.call( arguments, 1 );
+			this.each(function() {
+				var instance = $.data( this, 'simpleSlideshow' );
+				instance[ options ].apply( instance, args );
 			});
+		} 
+		else if ( typeof options === 'object' )
+		{
+			// initialize and attach to all the jquery objects passed in
+			this.each(function() {
+				var instance = $.data( this, 'simpleSlideshow' );
+				if ( !instance ) { // instance doesn't already exists so create it
+					$.data( this, 'simpleSlideshow', new $.simpleSlideshow( options, this ) );
+				}
+			});
+		}
+		else {
+			$.error( 'Method ' +  options + ' does not exist on jQuery.simpleSlideshow' );
+		}
+		return this; // return for chaining    
+	};
+	
+	// create a simpleSlideshow class
+	$.simpleSlideshow = function( options, element ) {
+		this.$el = $( element );
+		this._init( options ); // initialize
+	};
+	
+	
+	$.simpleSlideshow.prototype = {
+		// constructor
+		_init: function( options ){
+			this.options = $.extend( {
+				'slideshowData'			: [],
+				'slideFadeSpeed'		: 2000,
+				'slidePauseSpeed'		: 4000,
+				'paging'				: true,
+				'autostart'				: true
+			}, options );
 			
+			// get the number of additional images
+			this.slideshowlen = this.options.slideshowData.length;
+			this.$slides = [];
+			this.currentSlide = 1; // 1 based
 			
-			// get all images for the slideshow
-			var $slideshow = $this.find('div');
+			$this = this.$el; // the jQuery object referencing the slideshow div (passed in on init)
 			
-			// paging
-			var pagerID = $this[0].id + '-paging';
-			if (settings.paging) {
-				$pager = $('<div>').attr('id', pagerID);
-				$slideshow.each(function(ndx){
-					var slideIndex = ndx + 1;
-					$('<a data-slide=' + ndx + '>' + slideIndex + '</a>').attr({
-						style: 'cursor:pointer'
-					}).click(function(){
-						$slideshow.fadeOut(settings.slideFadeSpeed).eq($(this).data('slide')).fadeIn(settings.slideFadeSpeed);
-						clearInterval(t);
-					}).appendTo($pager);
+			// check that the array has items and the slideshow container exists in the page
+			if (this.slideshowlen > 0){
+				
+				// loop and append additional images
+				$.each(this.options.slideshowData, function(ndx, el){
+					if ($.isPlainObject(el)) {
+						var banner = '<img src="' + el.src + '" alt="' + el.alt + '">';
+						if (el.href) {
+							banner = '<a href="' + el.href + '">' + banner + '</a>';
+						}
+					} 
+					else {
+						var banner = el;
+					}
+					$placeholder = $('<div>').hide().append(banner);
+					$this.append($placeholder);
+					
 				});
-				$this.after($pager.show());
-				$pager.find('a:eq(0)').addClass('current-slide');
+				
+				
+				// get all images for the slideshow
+				this.$slides = $this.find('div');
+				
+				// paging
+				this.pagerID = $this[0].id + '-paging';
+				if (this.options.paging) {
+					$pager = $('<div>').attr('id', this.pagerID);
+					this.$slides.each(function(ndx){
+						var slideIndex = ndx + 1;
+						$('<a data-slide=' + ndx + '>' + slideIndex + '</a>').attr({
+							style: 'cursor:pointer'
+						}).click(function(){
+							
+						}).appendTo($pager);
+					});
+					$this.after($pager.show());
+					$pager.find('a:eq(0)').addClass('current-slide');
+				}
+				
+				// a really simple function to animate banners
+				//var slideOrdinal = 1;
+				
+				/*
+				changeSlide = function(){
+					slideOrdinal = slideOrdinal === $slideshow.length ? 1 : slideOrdinal + 1;
+					$slideshow.siblings().filter(':visible').fadeOut(this.options.slideFadeSpeed).end().eq(slideOrdinal - 1).fadeIn(this.options.slideFadeSpeed);
+					$('#'+pagerID+'>a').removeClass('current-slide').eq(slideOrdinal-1).addClass('current-slide');
+				};
+				*/
+				
+
 			}
 			
-			// a really simple function to animate banners
-			var slideOrdinal = 1;
-			changeSlide = function(){
-				slideOrdinal = slideOrdinal === $slideshow.length ? 1 : slideOrdinal + 1;
-				$slideshow.siblings().filter(':visible').fadeOut(settings.slideFadeSpeed).end().eq(slideOrdinal - 1).fadeIn(settings.slideFadeSpeed);
-				$('#'+pagerID+'>a').removeClass('current-slide').eq(slideOrdinal-1).addClass('current-slide');
-			};
+			if (this.options.autostart) {
+				this.start();
+			}
 			
-			// wait before firing
-			t = setInterval(changeSlide, settings.slidePauseSpeed);
+		},
+		
+		
+		destroy : function( ) {
+			return this.each(function(){
+				$(window).unbind('.simpleSlideshow');
+			})
+		},
+		
+
+		start : function( ){
+			var context = this; // note setInverval runs in the scope of the window so need to create an anonymous function so that a closure is created that contains a reference to the function
+			this.timer = setInterval(
+				function(){context.changeSlide();} 
+				, this.options.slidePauseSpeed
+			);
+		},
+		
+		changeSlide : function( slideOrdinal ){
+			slideOrdinal = slideOrdinal || this.currentSlide; // set default for optional argument
+			
+			this.currentSlide = slideOrdinal+1 > this.$slides.length ? 1 : slideOrdinal + 1;
+			var context = this;
+			this.$slides.siblings().filter(':visible').fadeOut(this.options.slideFadeSpeed).end().eq(this.currentSlide - 1).fadeIn(this.options.slideFadeSpeed, function(){
+				$('#'+context.pagerID+'>a').removeClass('current-slide').eq(context.currentSlide-1).addClass('current-slide');
+			});
+		},
+		
+		stop : function( ){
+			clearInterval(this.timer);
 		}
+		
+	}
+	
+	// API	
+	var methods = {
+		
+		
+
 	};
 	
 })( jQuery );
